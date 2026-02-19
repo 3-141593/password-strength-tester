4d2f28832ed96708bfa0e29943e29269c714c5df
3-141593 <commandmasterboi@gmail.com>
2026-02-19 13:05:11 +0900

    Fix formatting of ROCKYOU_FILES list

--- rockyou_config.py
+++ rockyou_config.py
@@ -1,6 +1,6 @@
 ROCKYOU_FILES = [
-    "wordlists/rockyou_sample.txt"
-   #"wordlists/rockyou1.txt"
-   #"wordlists/rockyou2.txt"
+    "wordlists/rockyou_sample.txt",
+   #"wordlists/rockyou1.txt",
+   #"wordlists/rockyou2.txt",
    #"wordlists/rockyou3.txt"
 ]
